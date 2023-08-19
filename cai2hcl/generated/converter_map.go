505784b65682ae69cbda386a9c5a138a5ad34e1b
Amir Romashkin <romashkin@google.com>
2023-08-19 21:28:39 +0000

    New version

--- cai2hcl/generated/converter_map.go
+++ cai2hcl/generated/converter_map.go
@@ -4,11 +4,13 @@ import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/generated/converters/common"
 	computeConverters "github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/generated/converters/compute"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta"
+	tpg "github.com/hashicorp/terraform-provider-google/google"
 )
 
 var ConverterNames = map[string]string{
 	computeConverters.ComputeForwardingRuleAssetType: "google_compute_forwarding_rule",
+	computeConverters.ComputeBackendServiceAssetType: "google_compute_backend_service",
+	computeConverters.ComputeHealthCheckAssetType:    "google_compute_health_check",
 }
 
 var ConverterMap map[string]common.Converter
@@ -18,6 +20,8 @@ func init() {
 
 	var factoryMap = map[string]func(schema map[string]*tfschema.Schema, name string) common.Converter{
 		"google_compute_forwarding_rule": computeConverters.NewComputeForwardingRuleConverter,
+		"google_compute_backend_service": computeConverters.NewComputeBackendServiceConverter,
+		"google_compute_health_check":    computeConverters.NewComputeHealthCheckConverter,
 	}
 
 	ConverterMap = make(map[string]common.Converter, len(factoryMap))
